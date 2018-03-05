499e2387bc25e33cfca800f11b68776a70496558
genreboy <javascriptjenkins@gmail.com>
2018-03-05 15:01:37 -0600

    add working 2 way ssl

--- src/main/java/springbootapi/spring/config/WebSecurityConfig.groovy
+++ src/main/java/springbootapi/spring/config/WebSecurityConfig.groovy
@@ -3,6 +3,7 @@ package springbootapi.spring.config
 import com.sun.net.httpserver.Filter
 import org.apache.catalina.filters.*
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.embedded.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -12,7 +13,11 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.authority.AuthorityUtils
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
 
 
 @Configuration
@@ -27,17 +32,11 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-
-//                .authorizeRequests()
-//                    .antMatchers("/admin/**").hasRole("ADMIN")
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin()
-//                .and()
-//                .httpBasic()
-        .x509()
-
-
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .x509()
+                .subjectPrincipalRegex($/CN=(.*?)(?:,|$)/$)
+                .userDetailsService(userDetailsService())
 
 
 
@@ -45,12 +44,31 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
-    @Autowired
-    void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-//                .inMemoryAuthentication()
-//                .withUser(env.getProperty("sample.username")).password(env.getProperty("sample.password")).roles("USER");
+//    @Autowired
+//    void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//        //... whose username is defined in the application's properties.
+////                .inMemoryAuthentication()
+////                .withUser(ALLOWED_USER).password("").roles("SSL_USER");
+////
+//
+////                .inMemoryAuthentication()
+////                .withUser(env.getProperty("sample.username")).password(env.getProperty("sample.password")).roles("USER");
+//
+//    }
 
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            UserDetails loadUserByUsername(String username) {
+                if (username.equals("cid")) {
+                    return new User(username, "",
+                            AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
+                }
+            }
+        }
     }
 
 
